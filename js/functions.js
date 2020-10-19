0bf16b2e5e61118281a024a43a1cc4bdc48c2539
Makika1996 <marijamarinkov96@gmail.com>
2020-10-19 14:37:17 +0200

    v1.5

--- js/functions.js
+++ js/functions.js
@@ -1,10 +1,10 @@
 $(document).ready(function () {
   console.log('circle');
 
-  circle($("#test-circle"), 80);
-  circle($("#test-circle2"), 95);
-  circle($("#test-circle3"), 60);
-  circle($("#test-circle4"), 65);
+  circle($("#test-circle"), 95);
+  circle($("#test-circle2"), 70);
+  circle($("#test-circle3"), 65);
+  circle($("#test-circle4"), 40);
 
   floatLabels();
 })
