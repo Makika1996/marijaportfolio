ae7ba7e54a98bf7ff7c447751742f777576aac48
Makika1996 <marijamarinkov96@gmail.com>
2020-10-19 17:45:01 +0200

    v1.7

--- js/functions.js
+++ js/functions.js
@@ -20,3 +20,11 @@ function circle(el, percent) {
     fillColor: "white"
   });
 }
+
+let mainNav = document.getElementById('js-menu');
+let navBarToggle = document.getElementById('js-navbar-toggle');
+
+navBarToggle.addEventListener('click', function () {
+
+  mainNav.classList.toggle('active');
+});
