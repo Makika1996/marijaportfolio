91a9f68b55c3d85f4c19f0f864908388c0ead12b
Makika1996 <marijamarinkov96@gmail.com>
2026-07-11 21:59:52 +0200

    v2.6

--- js/functions.js
+++ js/functions.js
@@ -1,19 +1,25 @@
 $(document).ready(function () {
-  console.log('circle');
+  if (typeof $.fn.circliful !== 'function') {
+    return;
+  }
 
   circle($("#test-circle"), 83);
   circle($("#test-circle2"), 46);
   circle($("#test-circle3"), 67);
   circle($("#test-circle4"), 38);
-})
+});
 
 function circle(el, percent) {
+  if (!el.length) {
+    return;
+  }
+
   el.circliful({
     animationStep: 2,
     foregroundBorderWidth: 5,
     backgroundBorderWidth: 0,
     percent: percent,
-    foregroundColor: "#edb5bf",
+    foregroundColor: "#8B0000",
     backgroundColor: "white",
     fillColor: "white"
   });
@@ -22,7 +28,8 @@ function circle(el, percent) {
 let mainNav = document.getElementById('js-menu');
 let navBarToggle = document.getElementById('js-navbar-toggle');
 
-navBarToggle.addEventListener('click', function () {
-
-  mainNav.classList.toggle('active');
-});
+if (mainNav && navBarToggle) {
+  navBarToggle.addEventListener('click', function () {
+    mainNav.classList.toggle('active');
+  });
+}
